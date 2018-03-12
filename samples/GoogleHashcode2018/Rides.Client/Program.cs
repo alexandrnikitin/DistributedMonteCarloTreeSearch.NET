85a32adf7ef2c8787f5310fdc9588e748e362de6
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-03-12 13:06:24 +0200

    Code

--- samples/GoogleHashcode2018/Rides.Client/Program.cs
+++ samples/GoogleHashcode2018/Rides.Client/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using DMCTS.GrainInterfaces;
+using Orleans;
+using Orleans.Runtime;
 
 namespace Rides.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return RunMainAsync().Result;
         }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await StartClientWithRetries())
+                {
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static async Task<IClusterClient> StartClientWithRetries(int initializeAttemptsBeforeFailing = 5)
+        {
+            int attempt = 0;
+            IClusterClient client;
+            while (true)
+            {
+                try
+                {
+                    var siloAddress = IPAddress.Loopback;
+                    var gatewayPort = 30000;
+                    client = new ClientBuilder()
+                        .ConfigureCluster(options => options.ClusterId = "helloworldcluster")
+                        .UseStaticClustering(options => options.Gateways.Add((new IPEndPoint(siloAddress, gatewayPort)).ToGatewayUri()))
+//                        .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IHello).Assembly).WithReferences())
+//                        .ConfigureLogging(logging => logging.AddConsole())
+                        .Build();
+
+                    await client.Connect();
+                    Console.WriteLine("Client successfully connect to silo host");
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    attempt++;
+                    Console.WriteLine($"Attempt {attempt} of {initializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
+                    if (attempt > initializeAttemptsBeforeFailing)
+                    {
+                        throw;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(4));
+                }
+            }
+
+            return client;
+        }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+            var tree = client.GetGrain<ITreeGrain<MakeRideAction>>(Guid.NewGuid());
+        }
+
+
+
     }
 }
--- samples/GoogleHashcode2018/Rides.Host/Program.cs
+++ samples/GoogleHashcode2018/Rides.Host/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
 
 namespace Rides.Host
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return RunMainAsync().Result;
         }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("Press Enter to terminate...");
+                Console.ReadLine();
+
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            // define the cluster configuration
+            var siloPort = 11111;
+            int gatewayPort = 30000;
+            var siloAddress = IPAddress.Loopback;
+            var builder = new SiloHostBuilder()
+                .Configure(options => options.ClusterId = "helloworldcluster")
+                .UseDevelopmentClustering(
+                    options => options.PrimarySiloEndpoint = new IPEndPoint(siloAddress, siloPort))
+                .ConfigureEndpoints(siloAddress, siloPort, gatewayPort);
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+
+
     }
 }
